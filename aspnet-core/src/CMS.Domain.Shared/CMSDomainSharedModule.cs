c5a035573eda5c4637eb0542406554f150543f89
Zhouqiulin <zql394@foxmail.com>
2020-03-10 21:34:40 +0800

    与ABP同步升级

--- aspnet-core/src/CMS.Domain.Shared/CMSDomainSharedModule.cs
+++ aspnet-core/src/CMS.Domain.Shared/CMSDomainSharedModule.cs
@@ -5,12 +5,12 @@ using Volo.Abp.FeatureManagement;
 using Volo.Abp.Identity;
 using Volo.Abp.IdentityServer;
 using Volo.Abp.Localization;
-using Volo.Abp.Localization.Resources.AbpValidation;
 using Volo.Abp.Modularity;
 using Volo.Abp.PermissionManagement;
 using Volo.Abp.SettingManagement;
 using Volo.Abp.TenantManagement;
 using Volo.Abp.VirtualFileSystem;
+using Volo.Abp.Validation.Localization;
 
 namespace CMS
 {
