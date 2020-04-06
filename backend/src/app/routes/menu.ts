d612e6d99d2a0a0931b3748ded8c2509a81a49c1
Zhouqiulin <zql394@foxmail.com>
2020-04-06 23:56:01 +0800

    添加菜单

--- backend/src/app/routes/menu.ts
+++ backend/src/app/routes/menu.ts
@@ -10,7 +10,77 @@ const headingMain = {
     heading: true
 };
 
+const articleManage={
+    text:'资讯管理',
+    link:'/article',
+    icon:'icon-note',
+    submenu: [
+        {
+            text: '资讯列表',
+            link: '/article/list'
+        },
+        {
+            text: '资讯类别',
+            link: '/article/type'
+        },
+
+    ]
+}
+const productManage={
+    text:'产品管理',
+    link:'/article',
+    icon:'icon-handbag',
+    submenu:[
+        {
+            text:'产品列表',
+            link:'/product/list'
+        },
+        {
+            text:'产品类别',
+            link:'/product/type'
+        }
+    ]
+}
+
+const pageManage={
+    text:'单页管理',
+    link:'/page',
+    icon:'icon-docs',
+    submenu:[
+        {
+            text:'单页列表',
+            link:'/page/list'
+        },
+        {
+            text:'单页类别',
+            link:'/page/type'
+        }
+    ]
+}
+const menuManage={
+    text:'菜单管理',
+    link:'/menu',
+    icon:'icon-menu',
+    submenu:[
+        {
+            text:'菜单列表',
+            link:'/product/list'
+        }
+    ]
+}
+
+const systemManage={
+    text:'系统管理',
+    link:'/tree',
+    icon:'icon-settings'
+}
+
 export const menu = [
     headingMain,
-    Home
+    Home,
+    articleManage,
+    productManage,
+    pageManage,
+    menuManage,
+    systemManage
 ];
