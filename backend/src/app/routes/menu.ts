63bcdcb3fde97c574ae86c13b0ebfc46a23f0078
zhouqiuln <zql@zql.me>
2020-11-09 20:34:36 +0800

    移除页面多余的元素

--- backend/src/app/routes/menu.ts
+++ backend/src/app/routes/menu.ts
@@ -1,85 +1,64 @@
-
-const Home = {
-    text: 'Home',
-    link: '/home',
-    icon: 'icon-home'
-};
-
-const headingMain = {
-    text: 'Main Navigation',
-    heading: true
-};
-
 const articleManage = {
-    text: '资讯管理',
-    link: '/articles',
-    icon: 'icon-note',
-    submenu: [
-        {
-            text: '资讯列表',
-            link: '/articles/list'
-        },
-        {
-            text: '新增资讯',
-            link: '/articles/details'
-        },
-
-    ]
+  text: '资讯管理',
+  link: '/articles',
+  icon: 'icon-note',
+  submenu: [
+    {
+      text: '资讯列表',
+      link: '/articles/list',
+    },
+    {
+      text: '新增资讯',
+      link: '/articles/details',
+    },
+  ],
 };
 const productManage = {
-    text: '产品管理',
-    link: '/article',
-    icon: 'icon-handbag',
-    submenu: [
-        {
-            text: '产品列表',
-            link: '/products/list'
-        },
-        {
-            text: '新增产品',
-            link: '/products/details'
-        }
-    ]
+  text: '产品管理',
+  link: '/article',
+  icon: 'icon-handbag',
+  submenu: [
+    {
+      text: '产品列表',
+      link: '/products/list',
+    },
+    {
+      text: '新增产品',
+      link: '/products/details',
+    },
+  ],
 };
 
 const pageManage = {
-    text: '单页管理',
-    link: '/pages',
-    icon: 'icon-docs',
-    submenu: [
-        {
-            text: '单页列表',
-            link: '/pages/list'
-        },
-        {
-            text: '新增单页',
-            link: '/pages/details'
-        }
-    ]
+  text: '单页管理',
+  link: '/pages',
+  icon: 'icon-docs',
+  submenu: [
+    {
+      text: '单页列表',
+      link: '/pages/list',
+    },
+    {
+      text: '新增单页',
+      link: '/pages/details',
+    },
+  ],
 };
-
 
 const systemManage = {
-    text: '系统管理',
-    link: '/system',
-    icon: 'icon-settings',
-    submenu: [
-        {
-            text: '菜单管理',
-            link: '/system/menu'
-        },
-        {
-            text: '分类管理',
-            link: '/system/tree'
-        }
-    ]
+  text: '系统管理',
+  link: '/system',
+  icon: 'icon-settings',
+  submenu: [
+    {
+      text: '菜单管理',
+      link: '/system/menu',
+    },
+    {
+      text: '分类管理',
+      link: '/system/tree',
+    },
+  ],
 };
 
-export const menu = [
-    headingMain,
-    Home,
-    articleManage,
-    productManage,
-    pageManage,
-    systemManage
-];
+export const menu = [articleManage, productManage, pageManage, systemManage];
